0810abcf9e832d525366df80184627ed3ec8f946
Nozomu Shibazaki <shibazaki_nozomu@ogis-ri.co.jp>
2011-08-02 16:55:34 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessageBoard::Application.config.secret_token = '4ed414d2d95bbdd734e26c5bd3f7984b79205fa4342210aa0c45c0a7c147a4c9c7777081f2e11c1796a59d6da489293e4c946322775c50b956dbed26bf652bac'
