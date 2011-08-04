150f4ab0a5d024f8304e98ee88dfdfe3dcd6c2fd
Nozomu Shibazaki <shibazaki_nozomu@ogis-ri.co.jp>
2011-08-04 19:14:50 +0900

    implement serarch test

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module MessageBoard
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    #config.encoding = "sjis"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
